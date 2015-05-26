876cc8319c9445b860a8302d200be879bcacf932
Jabaden <jabaden@mac.com>
2015-05-25 21:36:59 -0700

    One line test.

--- Project/public_html/object.js
+++ Project/public_html/object.js
@@ -6,6 +6,7 @@ var objects = [];
 var objSelected = -1;
 var defaultConeOrigin = [0,-10,0];
 var yAxis = [0,1,0];
+//THIS IS THE ONE LINE IM TRYING RIGHT NOW AT 9:36 PM
 
 // Object Class
 var Object = function(model_id, pos) {
